3ef3becbf6b7d886c08b2a39e66445c46162a7c0
NRgabi <gabinogueira805@gmail.com>
2025-05-04 18:07:31 +0100

    Added a simple print

--- src/gith/pkg2025007/GitH2025007.java
+++ src/gith/pkg2025007/GitH2025007.java
@@ -14,7 +14,9 @@ public class GitH2025007 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Gabi Nogueira - 2025007");
+       
+   
     }
     
 }
